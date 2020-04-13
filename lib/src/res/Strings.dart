4b97691dd03ff6f347bc24b07d98ccd5c572d8e2
Ilya Myasoedov <imiasoed@jcp.com>
2020-04-13 12:49:08 +0300

    fixed method body

--- lib/src/res/Strings.dart
+++ lib/src/res/Strings.dart
@@ -20,10 +20,7 @@ class Strings {
 
   Strings(this.locale);
 
-  static Strings of(BuildContext context) {
-    final l = Localizations.of(context, Strings);
-    return l;
-  }
+  static Strings of(BuildContext context) => Localizations.of(context, Strings)
 
   static Map<String, Map<String, String>> localizedValues = {
     'en': enMap,
